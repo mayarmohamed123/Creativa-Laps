c08a68c32fecea2e52ce86fd7c5d371075591326
mayar mohamed <mayar201500@gmail.com>
2024-08-17 15:15:17 +0300

    adding the first lap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import ClassComponent from "./Component/ClassComponent/ClassComponent";
 import FunctionComponent from "./Component/FunctionComponent/FunctionComponent";
